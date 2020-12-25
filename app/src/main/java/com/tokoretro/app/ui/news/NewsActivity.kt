acb09de59e233cd7b7330c886e6cdedabd7d113f
adithyaric <naughtyadit@gmail.com>
2020-12-25 22:31:09 +0700

    Rest Api

---
+++ app/src/main/java/com/tokoretro/app/ui/news/NewsActivity.kt
@@ -0,0 +1,16 @@
+package com.tokoretro.app.ui.news
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tokoretro.app.R
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
+    companion
+    object {
+        const val OPEN_NEWS = "open_news"
+    }
+}
---
+++ app/src/main/java/com/tokoretro/app/ui/news/NewsAdapter.kt
@@ -0,0 +1,23 @@
+package com.tokoretro.app.ui.news
+
+import android.content.Context
+import android.content.Intent
+import androidx.databinding.ViewDataBinding
+import com.bumptech.glide.Glide
+import com.tokoretro.app.R
+import com.tokoretro.app.data.model.News
+import com.tokoretro.app.databinding.ItemNewsBinding
+import com.tokoretro.app.ui.base.BaseAdapter
+
+class NewsAdapter(val context: Context) : BaseAdapter<News>(R.layout.item_news) {
+    override fun onBind(binding: ViewDataBinding?, data: News) {
+        val mBinding = binding as ItemNewsBinding
+        Glide.with(context).load(data.poster).into(mBinding.itemPoster)
+    }
+
+    override fun onClick(binding: ViewDataBinding?, data: News) {
+        val intent = Intent(context, NewsActivity::class.java)
+        intent.putExtra(NewsActivity.OPEN_NEWS, data)
+        context.startActivity(intent)
+    }
+}
